7cf0250793c819422a0e664d7e95c3fa585ddaea
Dylan Baker <dylanmbaker@yahoo.com>
2022-06-29 20:19:54 -0500

    created register functionality

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,28 +1,67 @@
-import React, { useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
+import { Link , useNavigate} from 'react-router-dom';
 
 import Header from '../components/Header';
 
 const Register = () => {
+    const [credentials, setCredentials] = useState({ email: '', password: '' });
+    const [error, setError] = useState('');
+    const userID = localStorage.getItem('id');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // axios.post('')
-  }
-  
-  return (
-    <div className='App'>
-      <Header />
-      <form className='auth-form' onSubmit={handleSubmit}>
-        <h1>Register</h1>
-        <input className='auth-input' type='email' placeholder='Email Address' />
-        <input className='auth-input' type='password' placeholder='Password' />
-        <button className='auth-btn'>Register</button>
-        <p>Been here before? <u><Link to='/'>Sign In.</Link></u></p>
-      </form>
-    </div>
-  )
-}
+    let navigateTo = useNavigate();
+
+    const handleChanges = (e) => {
+        setCredentials({ ...credentials, [e.target.name]: e.target.value });
+        setError('');
+    };
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        if (!credentials.email) {
+            setError('Please enter your email address');
+        } else if (!credentials.password) {
+          setError('Please enter a password');
+      } else {
+            axios
+                .post('https://dylan-todo-app-be.herokuapp.com/auth/register', credentials)
+                .then(() => {
+                  axios
+                  .post('https://dylan-todo-app-be.herokuapp.com/auth/signin', credentials)
+                  .then((res) => {
+                    console.log(res)
+                      localStorage.setItem('id', res.data.id);
+                      localStorage.setItem('token', res.data.token);
+                      navigateTo('../');
+                  })
+                  .catch((err) => {
+                      setError(err.response.data.message);
+                  });
+                })
+                .catch((err) => {
+                    setError(err.response.data.message);
+                });
+        }
+    };
+
+    return (
+        <div className='App'>
+            <Header />
+            <form className='auth-form' onSubmit={handleRegister}>
+                <h1>Register</h1>
+                {error && <p>{error}</p>}
+                <input className='auth-input' type='email' value={credentials.email} name='email' onChange={handleChanges} placeholder='Email Address' />
+                <input className='auth-input' type='password' value={credentials.password} name='password' onChange={handleChanges} placeholder='Password' />
+                <button className='auth-btn'>Register</button>
+                <p>
+                    Been here before?{' '}
+                    <u>
+                        <Link to='/'>Sign In.</Link>
+                    </u>
+                </p>
+            </form>
+        </div>
+    );
+};
 
 export default Register;
